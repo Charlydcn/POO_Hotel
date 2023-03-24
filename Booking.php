68b79897544db97e1f00697cfee9afda363f9d80
Charlydcn <Charly.ducournau@outlook.fr>
2023-03-24 09:14:13 +0100

    (Booking) Tested and checked all methods

--- Booking.php
+++ Booking.php
@@ -22,45 +22,45 @@
         // ************************************************ MÉTHODES ************************************************        
         // ************************************** ACCESSEURS (getters) **************************************
 
-        public function getClient() // A TESTER
+        public function getClient() // CHECK
         {
             return $this->_client;
         }
 
-        public function getHotel() // A TESTER
+        public function getHotel() // CHECK
         {
             return $this->_hotel;
         }
         
-        public function getRoom() // A TESTER
+        public function getRoom() // CHECK
         {
             return $this->_room;
         }
         
-        public function getDateStart() // A TESTER
+        public function getDateStart() // CHECK
         {
             return $this->_dateStart->format("d-m-Y");
         }
         
-        public function getDateEnd() // A TESTER
+        public function getDateEnd() // CHECK
         {
             return $this->_dateEnd->format("d-m-Y");
         }
 
-        public function getDuration() // A TESTER
+        public function getDuration() // CHECK
         {
             $interval = $this->_dateStart->diff($this->_dateEnd);
             return $interval->d . " jours";
         }
 
-        public function getPrice() // A TESTER
+        public function getPrice() // CHECK
         {
             $interval = $this->_dateStart->diff($this->_dateEnd);
             $price = $this->_room->getPrice() * $interval->d;
             return $price;
         }
         
-        public function getInfos()
+        public function getInfos() // CHECK
         {
             return "Réservation de " . $this->_client . "<br>"
             . "Hotel : " . $this->_hotel . "<br>"
@@ -71,7 +71,7 @@
             . "Prix : " . $this->getPrice();
         }
 
-        public function getInfosShort()
+        public function getInfosShort() // CHECK
         {
             return $this->_client . " - "
             . "Chambre " . $this->_room . " - "
@@ -82,34 +82,34 @@
         // ************************************************************************************************** 
         // ************************************** MUTATEURS (setters) ***************************************
 
-        public function setClient(Client $client)
+        public function setClient(Client $client) // CHECK
         {
             $this->_client = $client;
         }
 
-        public function setHotel(Hotel $hotel)
+        public function setHotel(Hotel $hotel) // CHECK
         {
             $this->_hotel = $hotel;
         }
 
-        public function setRoom(Room $room)
+        public function setRoom(Room $room) // CHECK
         {
             $this->_room = $room;
         }
 
-        public function setDateStart($dateStart)
+        public function setDateStart($dateStart) // CHECK
         {
             $this->_dateStart = new DateTime($dateStart);
         }
 
-        public function setDateEnd($dateEnd)
+        public function setDateEnd($dateEnd) // CHECK
         {
             $this->_dateEnd = new DateTime($dateEnd);
         }
 
         // ************************************************************************************************** 
 
-        public function __toString()
+        public function __toString() // CHECK
         {
             return $this->_client . " - "
             . "Chambre " . $this->_room->getId() . " - "
